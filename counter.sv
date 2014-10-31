fa97a0120df7490785cfddef9696f840e13e887a
vaiganmann <aovaigachev@gmail.com>
2014-10-31 19:52:17 +0300

    Испрваления
    Необходимо избегать использовать латчей.
    Об этом и сама альтера пишет в Recommended HDL Coding Styles:
    http://www.altera.com/literature/hb/qts/qts_qii51007.pdf
    "Altera recommends that you design without the use of latches whenever
    possible."
    
    Эту задачу можно решить без латча.
    
    2. Между сигналами "красный" и "красный и желтый" есть один такт,
    когда ни один из них не горит, но в реальной жизни такого нет.
    
    3. После асинхронного сброса светофор через какое-то время покажет
    зеленый вместе с красным и желтым, чего тоже нет в реальной
    жизни.
    
    4. Если поставить к примеру, period_red равный 100, то сто тактов не
    будет держаться
    красный.

--- counter.sv
+++ counter.sv
@@ -1,30 +1,25 @@
 `ifndef DEFS_COUNTER // if the already-compiled flag is not set...
   `define DEFS_COUNTER // set the flag 
-module counter (input bit clk, resetN,ready,
-				output bit [3:0] count,
-				input bit  [3:0] max_count,
+module counter (input bit clk, resetN,
+				output bit [10:0] count,
+				input bit  [10:0] max_count,
 				output bit overflow
 				);
 	
 logic enable; // internal enable signal for the counter
 
-always_latch begin // latch the ready input
-	if (resetN)
-		enable <= 0;
-	else if (ready)
-		enable <= 1;
-end
 	
 always_ff @(posedge clk or posedge resetN) begin // 5-bit counter
 	if (resetN) begin
 	 count <= 0;
 	 overflow <= 0;
 	end
-	else if ((count==max_count) && enable) begin
+	else if ((count==max_count  && count!=0)) begin
 		overflow <= 1;
-		count <= 0;
+		count   <= 0;
 	end 
-	else if(enable) begin
+	else 
+	 begin
 		count <= count + 1;
 		overflow <= 0;
 	end
--- work/counter/_primary.vhd
+++ work/counter/_primary.vhd
@@ -4,9 +4,8 @@ entity counter is
     port(
         clk             : in     vl_logic;
         resetN          : in     vl_logic;
-        ready           : in     vl_logic;
-        count           : out    vl_logic_vector(3 downto 0);
-        max_count       : in     vl_logic_vector(3 downto 0);
+        count           : out    vl_logic_vector(10 downto 0);
+        max_count       : in     vl_logic_vector(10 downto 0);
         overflow        : out    vl_logic
     );
 end counter;
